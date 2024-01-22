38bef3eaee8cd8d6aaca96cbabb9f3eca5fe7e87
Firman Handi Pratama <39254101+vearmunn@users.noreply.github.com>
2024-01-22 22:13:08 +0700

    Delete Covid Portofolio.sql

--- Covid Portofolio.sql
+++
@@ -1,7 +0,0 @@
-SELECT TOP (1000) [continent]
-      ,[location]
-      ,[date]
-      ,[population]
-      ,[new_vaccinations]
-      ,[RollingPeopleVaccinated]
-  FROM [PortofolioProject].[dbo].[PercentagePopulationVaccinated2]
